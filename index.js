7da3b728e7e662fc25c170ef99a99d93dcbe994f
Abdullah <abdullahsufyan2007@gmail.com>
2024-01-16 11:17:10 -0800

    commit

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ server.use("/cart", isAuth(), cartRouter.router);
 server.use("/orders", isAuth(), ordersRouter.router);
 server.use("/rating", isAuth(), ratingRouter.router);
 
+server.get((req, res) => res.sendFile(path.resolve("build", "index.html")));
 // Passport Strategies
 passport.use(
   "local",
